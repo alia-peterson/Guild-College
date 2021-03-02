cb43437a71b775265e2bb93b5df7d13211166708
Alia Peterson <alia@pnwebs.com>
2021-03-01 19:10:40 -0700

    Create mock student and course data

---
+++ src/data/course-data.js
@@ -0,0 +1,29 @@
+const courseData =  [
+  {
+    id: '0001',
+    name: 'Math 101',
+    time: 'MWF 9:00am-9:50am'
+  },
+  {
+    id: '0002',
+    name: 'Science 101',
+    time: 'TTh 9:00am-9:50am'
+  },
+  {
+    id: '0003',
+    name: 'Humanities 101',
+    time: 'MWF 10:00am-10:50am'
+  },
+  {
+    id: '0004',
+    name: 'Psychology 101',
+    time: 'MWF 11:00am-11:50am'
+  },
+  {
+    id: '0005',
+    name: 'English 101',
+    time: 'TTH 10:00am-10:50am'
+  }
+]
+
+export default courseData
---
+++ src/data/student-data.js
@@ -0,0 +1,8 @@
+const studentData = {
+  id: '210001',
+  name: 'Tony Stark',
+  email: 'tonystark@stark.com',
+  courses: []
+}
+
+export default studentData
